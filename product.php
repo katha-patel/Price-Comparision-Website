85ea7a67e329eca26a17e77ddb4bbc99525e0b64
Nikita Salunkhe <50489506+nikita-salunkhe@users.noreply.github.com>
2020-10-20 12:22:48 +0530

    updated product.php

--- product.php
+++ product.php
@@ -26,20 +26,21 @@ if ($err) {
   echo "cURL Error #:".$err;
 } else {
   $i = json_decode($response,true);
+
  ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>BHAago</title>
+  <title>shops</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="index.css">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
 </head>
 <body>
   <!-- Navigation bar-->
     <header>
-      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <nav class="nav__ navbar navbar-expand-lg navbar-dark bg-dark">
       <div class="container-fluid">
       <a class="navbar-brand" href="#">
       <img src="logo.jpeg" width="35" alt="logo">
@@ -151,6 +152,7 @@ foreach ($a as $b => $v) {
                   <div class="row padding">
                       <div class="card">
                         <img class="card-img-middle" src="<?php echo $va['product_store_logo']; ?>" >
+
                       </div>
                   </div>
                 </div>
@@ -165,11 +167,7 @@ foreach ($a as $b => $v) {
            <h6><?php echo "Delivery Cost : Rs ".$va['product_delivery_cost']."<br>"; ?></h6>
          </div>
          <div class="col-sm-2 col-md-2 col-xs-2 col-lg-2">
-           <br>
-           <br>
-           <br>
-           <br>
-           <br>
+
            <a href="<?php echo $va['product_store_url']."<br>";?>" class="btn btn-warning" target="_blank" >Buy Now</a>
          </div>
          </div>
@@ -196,34 +194,53 @@ foreach ($a as $b => $v) {
      <center>
        <br>
        <br>
-         <div class="mb-5 flex-center">
-           <!-- Facebook -->
-           <a class="fb-ic">
-             <i class="fab fa-facebook-f fa-lg white-text mr-md-5 mr-3 fa-2x" style="color:white;"> </i>
-           </a>
-           <!-- Twitter -->
-           <a class="tw-ic">
-             <i class="fab fa-twitter fa-lg white-text mr-md-5 mr-3 fa-2x" style="color:white;"> </i>
-           </a>
-           <!-- Google +-->
-           <a class="gplus-ic">
-             <i class="fab fa-google-plus-g fa-lg white-text mr-md-5 mr-3 fa-2x" style="color:white;"> </i>
-           </a>
-           <!--Linkedin -->
-           <a class="li-ic">
-             <i class="fab fa-linkedin-in fa-lg white-text mr-md-5 mr-3 fa-2x" style="color:white;"> </i>
-           </a>
-           <!--Instagram-->
-           <a class="ins-ic">
-             <i class="fab fa-instagram fa-lg white-text mr-md-5 mr-3 fa-2x" style="color:white;"> </i>
-           </a>
-           <!--Pinterest-->
-           <a class="pin-ic">
-             <i class="fab fa-pinterest fa-lg white-text fa-2x" style="color:white;"> </i>
-           </a>
-         </div>
-       </center>
-   </div>
+       <h2 style= "color:  #d1c5cd">Contact Us</h2>
+       <br>
+       <div class="mb-5 flex-center">
+         <!-- Facebook -->
+         <a class="fb-ic">
+           <i class="fab fa-facebook-f fa-lg white-text mr-md-5 mr-3 fa-2x" style="color:blue;"> </i>
+         </a>
+         <!-- Twitter -->
+         <a class="tw-ic">
+           <i class="fab fa-twitter fa-lg white-text mr-md-5 mr-3 fa-2x" style="color:blue;"> </i>
+         </a>
+         <!-- Google +-->
+         <a class="gplus-ic">
+           <i class="fab fa-google-plus-g fa-lg white-text mr-md-5 mr-3 fa-2x" style="color:green;"> </i>
+         </a>
+         <!--Linkedin -->
+         <a class="li-ic">
+           <i class="fab fa-linkedin-in fa-lg white-text mr-md-5 mr-3 fa-2x" style="color:blue;"> </i>
+         </a>
+         <!--Instagram-->
+         <a class="ins-ic">
+           <i class="fab fa-instagram fa-lg white-text mr-md-5 mr-3 fa-2x" style="color:pink;"> </i>
+         </a>
+         <!--Pinterest-->
+         <a class="pin-ic">
+           <i class="fab fa-pinterest fa-lg white-text fa-2x" style="color:white;"> </i>
+         </a>
+       </div>
+       <div class="row">
+         <div class="col-sm-4 col-md-4 col-xs-4 col-lg-4"></div>
+         <div class="col-sm-2 col-md-2 col-xs-2 col-lg-2">
+       <a class="phone">
+         <i class="fa fa-phone" aria-hidden="true" style="font-size:36px;color: #d1c5cd"> </i>
+         <br>
+         <h5 style="font-size:20px;color: #d1c5cd">777777777</h5>
+       </a>
+       </div>
+       <div class="col-sm-2 col-md-2 col-xs-2 col-lg-2">
+       <!--Pinterest-->
+       <a class="email">
+         <i class="fa fa-envelope" aria-hidden="true" style="font-size:36px;color: #d1c5cd"> </i>
+
+           <h6 style="font-size:20px;color: #d1c5cd">123@gmail.com</h6>
+       </a>
+       </div>
+       <div class="col-sm-4 col-md-4 col-xs-4 col-lg-4"></div>
+       </div>
    <!-- Copyright -->
    <div class="footer-copyright text-center py-3" style="color:white;">© 2020 Copyright: KN.com
    </div>
