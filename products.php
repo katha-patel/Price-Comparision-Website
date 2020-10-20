a17f056e018dd2d4de463e2b881987284889b9b7
Nikita Salunkhe <50489506+nikita-salunkhe@users.noreply.github.com>
2020-10-20 12:23:30 +0530

    updated products.php

--- products.php
+++ products.php
@@ -1,17 +1,9 @@
 <?php
 session_start();
 $curl = curl_init();
-if(isset($_POST['search']))
-{
-  $product=  $_POST['search1'];
-	$startprice= $_POST['Start-Price'];
-  $endprice= $_POST['End-Price'];
-  $_SESSION['product']=$product;
-  $_SESSION['startprice']=$startprice;
-  $_SESSION['endprice']=$endprice;
-}
+
 curl_setopt_array($curl, array(
-  CURLOPT_URL => "https://price-api.datayuge.com/api/v1/compare/search?api_key=suwVAxHngQ7O3a3TorWBKqSgFqNXlQqB27V&product=".$_SESSION['product']."&price_start=".$_SESSION['startprice']."&price_end=".$_SESSION['endprice']."&page=1",
+CURLOPT_URL => "https://price-api.datayuge.com/api/v1/compare/search?api_key=FesFoDCVzzZ6q7CE6QV4hlV0dkuLEEREKyo&product=".$_SESSION['pname']."&price_start=".$_SESSION['sprice']."&price_end=".$_SESSION['eprice']."&page=1",
   CURLOPT_RETURNTRANSFER => true,
   CURLOPT_ENCODING => "",
   CURLOPT_MAXREDIRS => 10,
@@ -29,7 +21,7 @@ curl_close($curl);
 if ($err) {
   echo "cURL Error #:".$err;
 } else {
-  $url = 'https://price-api.datayuge.com/api/v1/compare/search?api_key=suwVAxHngQ7O3a3TorWBKqSgFqNXlQqB27V&product='.$_SESSION['product'].'&price_start='.$_SESSION['startprice'].'&price_end='.$_SESSION['endprice'].'&page=1';
+  $url = "https://price-api.datayuge.com/api/v1/compare/search?api_key=FesFoDCVzzZ6q7CE6QV4hlV0dkuLEEREKyo&product=".$_SESSION['pname']."&price_start=".$_SESSION['sprice']."&price_end=".$_SESSION['eprice']."&page=1";
   $array = file_get_contents($url);
   $i = json_decode($array,true);
 }
@@ -37,16 +29,16 @@ if ($err) {
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>BHAago</title>
+  <title>Products</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="index.css">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
 </head>
 <body>
 <!-- Navigation bar-->
   <header>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav class="nav__ navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container-fluid">
     <a class="navbar-brand" href="#">
     <img src="logo.jpeg" width="35" alt="logo">
@@ -85,7 +77,7 @@ if ($err) {
 
       <div class="row">
         <div class="col-sm-1 col-md-1 bg-sucess"></div>
-        <div class="col-sm-3 col-md-3 bg-sucess">
+        <div class="col-sm-4 col-md-4 bg-sucess">
         <!-- card -->
         <div class="container-fluid padding">
           <div class="row padding">
@@ -96,7 +88,7 @@ if ($err) {
         </div>
         <br>
         </div>
-        <div class="col-sm-5 col-md-5 bg-sucess">
+        <div class="col-sm-4 col-md-4 bg-sucess">
 
           <h6><b><?php echo $i['data'][$k]['product_title']; ?></b></h6>
           <h6><?php echo "Lowest Price: Rs ".$i['data'][$k]['product_lowest_price'];?></h6>
@@ -126,34 +118,53 @@ if ($err) {
       <center>
         <br>
         <br>
-          <div class="mb-5 flex-center">
-            <!-- Facebook -->
-            <a class="fb-ic">
-              <i class="fab fa-facebook-f fa-lg white-text mr-md-5 mr-3 fa-2x" style="color:white;"> </i>
-            </a>
-            <!-- Twitter -->
-            <a class="tw-ic">
-              <i class="fab fa-twitter fa-lg white-text mr-md-5 mr-3 fa-2x" style="color:white;"> </i>
-            </a>
-            <!-- Google +-->
-            <a class="gplus-ic">
-              <i class="fab fa-google-plus-g fa-lg white-text mr-md-5 mr-3 fa-2x" style="color:white;"> </i>
-            </a>
-            <!--Linkedin -->
-            <a class="li-ic">
-              <i class="fab fa-linkedin-in fa-lg white-text mr-md-5 mr-3 fa-2x" style="color:white;"> </i>
-            </a>
-            <!--Instagram-->
-            <a class="ins-ic">
-              <i class="fab fa-instagram fa-lg white-text mr-md-5 mr-3 fa-2x" style="color:white;"> </i>
-            </a>
-            <!--Pinterest-->
-            <a class="pin-ic">
-              <i class="fab fa-pinterest fa-lg white-text fa-2x" style="color:white;"> </i>
-            </a>
-          </div>
-        </center>
-    </div>
+        <h2 style= "color:  #d1c5cd">Contact Us</h2>
+        <br>
+        <div class="mb-5 flex-center">
+          <!-- Facebook -->
+          <a class="fb-ic">
+            <i class="fab fa-facebook-f fa-lg white-text mr-md-5 mr-3 fa-2x" style="color:blue;"> </i>
+          </a>
+          <!-- Twitter -->
+          <a class="tw-ic">
+            <i class="fab fa-twitter fa-lg white-text mr-md-5 mr-3 fa-2x" style="color:blue;"> </i>
+          </a>
+          <!-- Google +-->
+          <a class="gplus-ic">
+            <i class="fab fa-google-plus-g fa-lg white-text mr-md-5 mr-3 fa-2x" style="color:green;"> </i>
+          </a>
+          <!--Linkedin -->
+          <a class="li-ic">
+            <i class="fab fa-linkedin-in fa-lg white-text mr-md-5 mr-3 fa-2x" style="color:blue;"> </i>
+          </a>
+          <!--Instagram-->
+          <a class="ins-ic">
+            <i class="fab fa-instagram fa-lg white-text mr-md-5 mr-3 fa-2x" style="color:pink;"> </i>
+          </a>
+          <!--Pinterest-->
+          <a class="pin-ic">
+            <i class="fab fa-pinterest fa-lg white-text fa-2x" style="color:white;"> </i>
+          </a>
+        </div>
+        <div class="row">
+          <div class="col-sm-4 col-md-4 col-xs-4 col-lg-4"></div>
+          <div class="col-sm-2 col-md-2 col-xs-2 col-lg-2">
+        <a class="phone">
+          <i class="fa fa-phone" aria-hidden="true" style="font-size:36px;color: #d1c5cd"> </i>
+          <br>
+          <h5 style="font-size:20px;color: #d1c5cd">777777777</h5>
+        </a>
+        </div>
+        <div class="col-sm-2 col-md-2 col-xs-2 col-lg-2">
+        <!--Pinterest-->
+        <a class="email">
+          <i class="fa fa-envelope" aria-hidden="true" style="font-size:36px;color: #d1c5cd"> </i>
+
+            <h6 style="font-size:20px;color: #d1c5cd">123@gmail.com</h6>
+        </a>
+        </div>
+        <div class="col-sm-4 col-md-4 col-xs-4 col-lg-4"></div>
+        </div>
     <!-- Copyright -->
     <div class="footer-copyright text-center py-3" style="color:white;">© 2020 Copyright: KN.com
     </div>
